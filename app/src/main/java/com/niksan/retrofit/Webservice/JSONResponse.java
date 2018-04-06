c5b1ce23c4d7240fa617ea167c8df81c6b59a81a
Amirhosein Heydari <mramir2008@gmail.com>
2018-04-06 19:02:40 +0430

    Initial commit

---
+++ app/src/main/java/com/niksan/retrofit/Webservice/JSONResponse.java
@@ -0,0 +1,11 @@
+package com.niksan.retrofit.Webservice;
+
+import com.niksan.retrofit.Webservice.AndroidVersion;
+
+public class JSONResponse {
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+}
---
+++ app/src/main/java/com/niksan/retrofit/Webservice/RequestInterface.java
@@ -0,0 +1,10 @@
+package com.niksan.retrofit.Webservice;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RequestInterface {
+
+    @GET("android/jsonandroid")
+    Call<JSONResponse> getJSON();
+}
